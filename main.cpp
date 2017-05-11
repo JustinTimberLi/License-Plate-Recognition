862d3711fedda27b26014bf54b7f54a830a56498
cugwyman <435059696@qq.com>
2017-05-11 14:55:55 +0800

    Revert "opencv 德莫、"
    This reverts commit d8bf0008a97ca911e335ddd7717c2c4b80c27385.

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include<opencv2\opencv.hpp>
-using namespace cv;
-int main()
-{
-	Mat picture = imread("ิมB0K999.jpg");//picture must be placed in project
-	imshow("demo", picture);
-	waitKey(20170511);
-}
